06028c312b2092c50277a8eed1f7798b05ff6f5c
moris96 <moriskhoudari@gmail.com>
2023-01-22 14:06:51 -0500

    auth page created

---
+++ src/pages/AuthPage/AuthPage.js
@@ -0,0 +1,14 @@
+//Auth Page
+
+import SignUpForm from "../components/SignUpForm/SignUpForm"
+import LoginForm from "../components/LoginForm/LoginForms"
+
+export default function AuthPage(props){
+    return(
+        <main>
+            <h1>Auth Page</h1>
+            <SignUpForm setUser={props.setUser}/>
+            <LoginForm setUser={props.setUser} />
+        </main>
+    )
+};
