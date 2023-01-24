12bbe8406ba11fe448ce5507bed7bb73646d8628
MichaelOb89 <michaelobrien.t@hotmail.com>
2023-01-23 20:13:11 -0500

    Added Component to create comments

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,14 +1,17 @@
 import { useState } from "react"
+import NewComment from "../NewComment/NewComment"
 
 export default function Post({post, user}){
+    const [showComments, setShowComment] = useState(null)
+    const [createComment, setCreateComment] = useState(false)
 
-    
     return(
         <div className="post">
             <h1>{post.title}</h1>
             <h3>{post.text}</h3>
             {post.imgLink ? <img src={post.imgLink}/> : ""}
-            <button>Add Note</button>
+            <button onClick={()=>setCreateComment(!createComment)}>Add Note</button>
+            {createComment? <NewComment />:""}
             <button>Share</button>
             <button>Like</button>
             {user == post.creator ? 
