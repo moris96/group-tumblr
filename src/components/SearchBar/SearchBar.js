0e22227b7fbb97c824bf4dd685592cc0865449bb
jerricklc@gmail.com <jerricklc@gmail.com>
2023-01-23 20:57:13 -0500

    created searchbar component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,10 @@
+
+
+export default function SearchBar () {
+  return (
+    <form className="search">
+      <input type="text" placeholder="Search Tumblr" />
+      <button type="submit" >Search</button>
+    </form>
+  )
+}
