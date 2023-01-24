890a7f73eeb6cca92e3f802ea995925948fe0734
jerricklc@gmail.com <jerricklc@gmail.com>
2023-01-23 20:37:11 -0500

    attempt to add ternary functionality

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -54,8 +54,6 @@ export default class SignUpForm extends Component {
                 <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required /><br/>
                 <label>Confirm: </label>
                 <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
-
-               
                 <button type="submit" disabled={disable}>SIGN UP</button>
               </form>
             </div>
--- src/components/SuggestedFollows/SuggestedFollows.js
+++ src/components/SuggestedFollows/SuggestedFollows.js
@@ -3,6 +3,8 @@ import styles from "../SuggestedFollows/SuggestedFollows.module.scss"
 
 export default function SuggestedFollows () {
   return (
-    <h1 className={styles.suggestedContainer}>Suggested Follows Component</h1>
+    <h1 className={styles.suggestedContainer}>
+      Suggested Follows Component
+    </h1>
   )
 }
