f0c2d2870a08c9627083f3815eefd7b4de418a7a
moris96 <moriskhoudari@gmail.com>
2023-01-24 20:20:27 -0500

    working on navbar styles

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@
 
 export default function SearchBar () {
   return (
-    <form className="search">
+    <form className="search-bar">
       <input type="text" placeholder="Search Tumblr" />
       <button type="submit" >Search</button>
     </form>
