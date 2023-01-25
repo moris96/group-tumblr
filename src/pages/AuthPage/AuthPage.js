cde7ac095a61cfe2e7634c6197aab89c342e0c9a
jerricklc@gmail.com <jerricklc@gmail.com>
2023-01-25 18:56:31 -0500

    added popup module to auth page

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,25 +1,41 @@
 import "./AuthPage.css"
-import SingupPage from "./SignupPage";
+import SignUpPage from "./SignupPage"
 import LoginPage from "./LoginPage"
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 // import { Routes, Route } from "react-router-dom"
+import Popup from "reactjs-popup";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import LoginForm from "../../components/LogInForm/LogInForm";
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
 
 export default function AuthPage(props){
     return(
         <main id="Auth">
             <nav>
             <Link to="/">Home</Link>
-            <Link to="/login">Log In</Link>
-            <Link to="/signup">Sign Up</Link>
+            <Popup trigger={<button className="logIn">Log In</button>}
+            modal
+            nested
+            >
+            {<LoginForm />}
+            </Popup>
+            <Popup trigger={<button className="SignUp">Sign Up</button>}
+            modal
+            nested
+            >{<SignUpForm />}</Popup>
             </nav>
             <Routes>
                 <Route path="/" element={<></>} />
                 <Route path="/login" element={<LoginPage setUser={props.setUser} />} />
-                <Route path="/signup" element={<SingupPage setUser={props.setUser} />} />
+                <Route path="/signup" element={<SignUpPage setUser={props.setUser} />} />
             </Routes>
             {/* <LoginPage setUser={props.setUser}/> */}
+            <section>
+                <body>
+                    <h1>HELLO</h1>
+                </body>
+            </section>
         </main>
     )
 }
