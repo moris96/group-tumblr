6f1fc23d3f6e764237dc99af92acdb53b907c24b
MichaelOb89 <michaelobrien.t@hotmail.com>
2023-01-24 20:46:53 -0500

    Added functionality to comment on posts. Just need to add the populate method

--- src/components/NewComment/NewComment.js
+++ src/components/NewComment/NewComment.js
@@ -1,19 +1,54 @@
 import { useState } from "react"
 
-export default function NewComment({post}){
-    const [newComment, setNewComment] = useState(null)
-    const createComment = async () =>{
-        const response = await fetch(`/api/comments`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({...newComment})
-        })
+export default function NewComment({post, createComment, setCreateComment}){
+    const [newComment, setNewComment] = useState({
+        postId: post._id,
+        text: "",
+        likes: 0
+    })
+
+
+    const createComment2 = async () =>{
+        try {
+            const response = await fetch(`/api/comments`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({...newComment})
+            })
+            const data = await response.json()
+            const allPosts = [...post.notes,data._id]
+            console.log(allPosts)
+            const response2 = await fetch(`/api/posts/${post._id}`,{
+                method: "PUT",
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({notes: allPosts})
+            })
+            setNewComment({
+                postId: post._id,
+                text: "",
+                likes: 0
+            })
+            setCreateComment(!createComment)
+        } catch (error) {
+            console.error(error)
+        }
+
+
+    }
+    const handleChange = (evt) => {
+        setNewComment({...NewComment, [evt.target.name]: evt.target.value,
+        postId: post._id,
+        likes: 0})
     }
     return(
         <>
             <h2>New Comment here</h2>
+            <input type="text" value={newComment.text} name='text' onChange={handleChange}></input>
+            <input type="submit" value="Submit comment" onClick={createComment2}></input>
         </>
     )
 }
