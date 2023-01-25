15cb56b3344eeb3ac067c453502547c251ecaa5d
jerricklc@gmail.com <jerricklc@gmail.com>
2023-01-24 20:47:23 -0500

    added settings to account popup

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,11 +1,13 @@
 
 import Logout from "../LogOut/LogOut"
+import Settings from "../../pages/Settings/Settings"
 
 export default function Account () {
   return (
-    <>
+    <main>
       <h1>Account</h1>
       <Logout />
-    </>
+      <Settings />
+    </main>
   )
 }
