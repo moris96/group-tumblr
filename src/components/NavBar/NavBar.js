b7c85444b4f038212b1675035c342abf568b3a3f
moris96 <moriskhoudari@gmail.com>
2023-01-25 20:06:32 -0500

    updated styles for navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -22,12 +22,12 @@ export default function NavBar(props){
 
       <Logo />
       <SearchBar />
-      <Link to='/'><button className="home-button"><img className="home-icon" src="https://static.thenounproject.com/png/77002-200.png" alt="icon" /></button></Link>&nbsp;
-      <button className="explore-button"><img className="explore-icon" src="https://t3.ftcdn.net/jpg/03/16/35/26/360_F_316352607_hAJzMbFFXKW3EsB9vL51KuKh50MwekEC.jpg" alt="explore" /></button>&nbsp;
-      <button className="mail-button"><img className="mail-icon" src="https://img.freepik.com/free-icon/envelope_318-408141.jpg?w=2000" alt="explore" /></button>&nbsp;
-      <button className="chat-button"><img className="chat-icon" src="https://cdn-icons-png.flaticon.com/512/40/40284.png" alt="chat" /></button>&nbsp;
-      <Popup trigger={<button className="account-button"><img className="account-icon" src="https://static.thenounproject.com/png/4038155-200.png" alt="account" /></button>} >{<Account />}</Popup>
-      <button className="post-button"><img className="post-icon" src="https://www.pngkit.com/png/detail/436-4368614_png-file-new-post-icon-png.png" alt="post" /></button>&nbsp;
+      <Link to='/'><button className="home-button"><img className="home-icon" src={process.env.PUBLIC_URL+"/iconsImg/8.png"} alt="icon" /></button></Link>&nbsp;
+      <button className="explore-button"><img className="explore-icon" src={process.env.PUBLIC_URL+"/iconsImg/7.png"} alt="explore" /></button>&nbsp;
+      <button className="mail-button"><img className="mail-icon" src={process.env.PUBLIC_URL+"/iconsImg/5.png"} alt="explore" /></button>&nbsp;
+      <button className="chat-button"><img className="chat-icon" src={process.env.PUBLIC_URL+"/iconsImg/4.png"} alt="chat" /></button>&nbsp;
+      <Popup trigger={<button className="account-button"><img className="account-icon" src={process.env.PUBLIC_URL+"/iconsImg/2.png"} alt="account" /></button>} >{<Account />}</Popup>
+      <button className="post-button"><img className="post-icon" src={process.env.PUBLIC_URL+"/iconsImg/1.png"} alt="post" /></button>&nbsp;
     </div>
     )
 }
