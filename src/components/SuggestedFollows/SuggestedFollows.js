7e75380f55a07773aac0291fa43a7ff30e63055c
jerricklc@gmail.com <jerricklc@gmail.com>
2023-01-28 23:23:39 -0500

    changed favicon, html tab title, and added lego head for use as default profile picture

--- src/components/SuggestedFollows/SuggestedFollows.js
+++ src/components/SuggestedFollows/SuggestedFollows.js
@@ -2,30 +2,33 @@ import styles from "../SuggestedFollows/SuggestedFollows.module.scss"
 import { useState, useEffect } from 'react'
 
 export default function SuggestedFollows () {
-  const [showUsers, setShowUsers] = useState(null)
-  const getShowUsers = async () => {
+  const [users, setUsers] = useState(null)
+  const getUsers = async () => {
     try {
       const response = await fetch(`/api/users`)
       const data = await response.json()
-      setShowUsers(data)
+      setUsers(data)
     } catch (error) {
       console.error(error)
     }
   }
 
   useEffect(() => {
-    getShowUsers()
+    getUsers()
   }, [])
 
 
   return (
-    <div>
-      <h1 className={styles.suggestedContainer}>
-        Suggested Follows Component
+    <div className={styles.suggestedContainer}>
+      <h1 classsName={styles.suggestedTitle}>
+        Check out these blogs
       </h1>
-      {showUsers ? showUsers.map((showUser) => {
-        return <p key={showUser.id}>{showUser.username}</p>
-      }): "No users to show"}
+      <hr/>
+        {users ? users.map((user) => {
+          return <p key={user.id}>
+            {user.username}
+            </p>
+        }): "No users to show"}
     </div>
   )
 }
